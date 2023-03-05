2e50a468ac67712dfdf96b2ee5dca956bdef42dd
HasegawaShuhei <sh1027.dev@gmail.com>
2023-03-05 16:20:08 +0900

    supabase接続

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: dotenv.get('PROJECT_URL'),
+    anonKey: dotenv.get('API_KEY'),
+  );
   runApp(const MyApp());
 }
 
